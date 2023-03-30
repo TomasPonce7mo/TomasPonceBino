ec5aeeaf23bc729332604c541cfa019b7fe70734
TomasPonce7mo <128190931+TomasPonce7mo@users.noreply.github.com>
2023-03-30 17:22:25 -0300

    Add files via upload

---
+++ multiplos.js
@@ -0,0 +1,32 @@
+function ContarMultiplosde2(n){
+    if(n % 2 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function ContarMultiplosde3(n){
+    if(n % 3 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function ContarMultiplosde4(n){
+    if(n % 4 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function ContarMultiplosde5(n){
+    if(n % 5 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
